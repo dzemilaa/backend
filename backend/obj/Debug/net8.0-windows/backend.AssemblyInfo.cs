3a946e577b653178af379b1f65e942c46d2a57c0
dzemilaa <dzemila2010@gmail.com>
2025-02-06 02:22:13 +0100

    add

--- backend/obj/Debug/net8.0-windows/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0-windows/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fcdc8cefdde00107af76bf014854b9571e8216f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
